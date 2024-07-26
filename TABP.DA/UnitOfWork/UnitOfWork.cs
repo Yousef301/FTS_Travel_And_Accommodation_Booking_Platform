8f9b59f1e5bf79b3e7bc903bd707f4f09aef208d
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-07-26 15:26:42 +0300

    Create UnitOfWork

---
+++ TABP.DA/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,59 @@
+﻿using System.Data;
+using Microsoft.EntityFrameworkCore;
+using TABP.DAL.Interfaces;
+
+namespace TABP.DAL.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly TABPDbContext _context;
+
+    public UnitOfWork(TABPDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task BeginTransactionAsync()
+    {
+        await _context.Database.BeginTransactionAsync();
+    }
+
+    public async Task CommitTransactionAsync()
+    {
+        if (_context.Database.CurrentTransaction is null) return;
+
+        await _context.Database.CommitTransactionAsync();
+    }
+
+    public async Task RollbackTransactionAsync()
+    {
+        if (_context.Database.CurrentTransaction is null) return;
+
+        await _context.Database.RollbackTransactionAsync();
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        UpdateAuditEntities();
+
+        return await _context.SaveChangesAsync();
+    }
+
+    private void UpdateAuditEntities()
+    {
+        _context.ChangeTracker.DetectChanges();
+
+        foreach (var entry in _context.ChangeTracker.Entries<IAuditableEntity>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedAt = DateTime.Now;
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.ModifiedAt = DateTime.Now;
+                    break;
+            }
+        }
+    }
+}
