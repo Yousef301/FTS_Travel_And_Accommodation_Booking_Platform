4dc934d021f2933e737a5134775ed9bfcfaa1520
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-07-26 15:27:39 +0300

    Add DbContext and Services configuration for DAL

---
+++ TABP.DA/DALServicesConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TABP.DAL.Interfaces;
+using TABP.DAL.Interfaces.Repositories;
+using TABP.DAL.Repositories;
+
+namespace TABP.DAL;
+
+public static class DALServicesConfiguration
+{
+    internal static IServiceCollection BuildInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext(configuration)
+            .AddRepositories();
+
+        return services;
+    }
+
+    private static IServiceCollection AddDbContext(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<TABPDbContext>(options =>
+        {
+            options.UseSqlServer(configuration["ConnectionStrings:TABPDb"]);
+        });
+
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IAmenityRepository, AmenityRepository>()
+            .AddScoped<IBookingDetailRepository, BookingDetailRepository>()
+            .AddScoped<IBookingRepository, BookingRepository>()
+            .AddScoped<ICityImageRepository, CityImageRepository>()
+            .AddScoped<ICityRepository, CityRepository>()
+            .AddScoped<ICredentialRepository, CredentialRepository>()
+            .AddScoped<IHotelImageRepository, HotelImageRepository>()
+            .AddScoped<IHotelRepository, HotelRepository>()
+            .AddScoped<IInvoiceRepository, InvoiceRepository>()
+            .AddScoped<IPaymentRepository, PaymentRepository>()
+            .AddScoped<IReviewRepository, ReviewRepository>()
+            .AddScoped<IRoomAmenityRepository, RoomAmenityRepository>()
+            .AddScoped<IRoomImageRepository, RoomImageRepository>()
+            .AddScoped<IRoomRepository, RoomRepository>()
+            .AddScoped<ISpecialOfferRepository, SpecialOfferRepository>()
+            .AddScoped<IUserRepository, UserRepository>()
+            .AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
+
+        return services;
+    }
+}
---
+++ TABP.DA/TABPDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TABP.DAL.Configurations;
+using TABP.DAL.Entities;
+
+namespace TABP.DAL;
+
+public class TABPDbContext : DbContext
+{
+    public DbSet<Amenity> Amenities { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<BookingDetail> BookingDetails { get; set; }
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Credential> Credentials { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<CityImage> CityImages { get; set; }
+    public DbSet<HotelImage> HotelImages { get; set; }
+    public DbSet<RoomImage> RoomImages { get; set; }
+    public DbSet<Invoice> Invoices { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<RoomAmenity> RoomAmenities { get; set; }
+    public DbSet<SpecialOffer> SpecialOffers { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost;Database=TABP;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new AmenityConfiguration());
+        modelBuilder.ApplyConfiguration(new BookingConfiguration());
+        modelBuilder.ApplyConfiguration(new BookingDetailConfiguration());
+        modelBuilder.ApplyConfiguration(new CityConfiguration());
+        modelBuilder.ApplyConfiguration(new CredentialConfiguration());
+        modelBuilder.ApplyConfiguration(new HotelConfiguration());
+        modelBuilder.ApplyConfiguration(new CityImageConfiguration());
+        modelBuilder.ApplyConfiguration(new RoomImageConfiguration());
+        modelBuilder.ApplyConfiguration(new HotelImageConfiguration());
+        modelBuilder.ApplyConfiguration(new InvoiceConfiguration());
+        modelBuilder.ApplyConfiguration(new PaymentConfiguration());
+        modelBuilder.ApplyConfiguration(new ReviewConfiguration());
+        modelBuilder.ApplyConfiguration(new RoomAmenityConfiguration());
+        modelBuilder.ApplyConfiguration(new RoomConfiguration());
+        modelBuilder.ApplyConfiguration(new SpecialOfferConfiguration());
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+    }
+}
