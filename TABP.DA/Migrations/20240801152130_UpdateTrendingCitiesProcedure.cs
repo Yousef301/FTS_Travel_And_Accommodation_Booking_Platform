765aefaae5003f8f87eb3dd0a07708c18ed5fe9e
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-08-01 23:57:22 +0300

    Update trending cities procedure

---
+++ TABP.DA/Migrations/20240801152130_UpdateTrendingCitiesProcedure.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TABP.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTrendingCitiesProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE IF EXISTS SP_GetTrendingCities");
+            migrationBuilder.Sql(
+                @"CREATE PROCEDURE SP_GetTrendingCities
+                  AS
+                  BEGIN
+                      WITH CityBookingCounts AS (
+                          SELECT 
+                              c.Id,
+                              c.Name AS CityName,
+                              COUNT(b.Id) AS BookingCount
+                          FROM 
+                              bookings b
+                          JOIN 
+                              hotels h ON b.HotelId = h.Id
+                          JOIN 
+                              cities c ON h.CityId = c.Id
+                          GROUP BY 
+                              c.Id, c.Name
+                      )
+                      SELECT 
+                          TOP 5
+                          Id,
+                          CityName
+                      FROM 
+                          CityBookingCounts
+	                  ORDER BY 
+                          BookingCount DESC;
+                  END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"DROP PROCEDURE SP_GetTrendingCities");
+        }
+    }
+}
