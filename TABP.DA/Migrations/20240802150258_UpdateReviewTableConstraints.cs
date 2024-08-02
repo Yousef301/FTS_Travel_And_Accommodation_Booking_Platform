be6c2edd83f8adac334eabf9a17af235da292a56
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-08-02 21:00:01 +0300

    Update reviews table constraints
    Each user now can review the same hotel only once, but he can update it

---
+++ TABP.DA/Migrations/20240802150258_UpdateReviewTableConstraints.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TABP.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateReviewTableConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews");
+
+            migrationBuilder.UpdateData(
+                table: "Credentials",
+                keyColumn: "Id",
+                keyValue: new Guid("7e8b9d0a-cf4a-4a22-b7a5-7d8b2e9a6f0c"),
+                column: "CreatedAt",
+                value: new DateTime(2024, 8, 2, 18, 2, 57, 584, DateTimeKind.Local).AddTicks(1561));
+
+            migrationBuilder.UpdateData(
+                table: "Credentials",
+                keyColumn: "Id",
+                keyValue: new Guid("a3c9b0a8-d7e6-4d1c-bb9d-4d2c3bde7a1e"),
+                column: "CreatedAt",
+                value: new DateTime(2024, 8, 2, 18, 2, 57, 584, DateTimeKind.Local).AddTicks(1557));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("d9b4bcca-9d5b-4f3d-bf89-7a367becfbd2"),
+                column: "CreatedAt",
+                value: new DateTime(2024, 8, 2, 18, 2, 57, 585, DateTimeKind.Local).AddTicks(5205));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("e7b7c08e-4c3a-41f5-9a9d-8571b2e4a5f4"),
+                column: "CreatedAt",
+                value: new DateTime(2024, 8, 2, 18, 2, 57, 585, DateTimeKind.Local).AddTicks(5210));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserId_HotelId",
+                table: "Reviews",
+                columns: new[] { "UserId", "HotelId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_UserId_HotelId",
+                table: "Reviews");
+
+            migrationBuilder.UpdateData(
+                table: "Credentials",
+                keyColumn: "Id",
+                keyValue: new Guid("7e8b9d0a-cf4a-4a22-b7a5-7d8b2e9a6f0c"),
+                column: "CreatedAt",
+                value: new DateTime(2024, 8, 2, 13, 19, 46, 915, DateTimeKind.Local).AddTicks(6745));
+
+            migrationBuilder.UpdateData(
+                table: "Credentials",
+                keyColumn: "Id",
+                keyValue: new Guid("a3c9b0a8-d7e6-4d1c-bb9d-4d2c3bde7a1e"),
+                column: "CreatedAt",
+                value: new DateTime(2024, 8, 2, 13, 19, 46, 915, DateTimeKind.Local).AddTicks(6740));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("d9b4bcca-9d5b-4f3d-bf89-7a367becfbd2"),
+                column: "CreatedAt",
+                value: new DateTime(2024, 8, 2, 13, 19, 46, 916, DateTimeKind.Local).AddTicks(8333));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("e7b7c08e-4c3a-41f5-9a9d-8571b2e4a5f4"),
+                column: "CreatedAt",
+                value: new DateTime(2024, 8, 2, 13, 19, 46, 916, DateTimeKind.Local).AddTicks(8339));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews",
+                column: "UserId");
+        }
+    }
+}
